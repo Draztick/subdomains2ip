ca7914ec28c81f010a15c348c931cc001e48e6fd
Draztick <draztick@draztick.com>
2025-04-11 15:10:25 -0400

    changed echo to work with coloring

--- subdomains2ip.sh
+++ subdomains2ip.sh
@@ -5,6 +5,6 @@ NC='\033[0m';
 
 for i in $(cat $1); 
 do 
-	echo "${GREEN}=======$i=======${NC}" >> $2;
-	host $i | grep "has address" | awk -F" " '{ print $4 }' >> $2;
+	echo -e "$GREEN=======$i=======$NC" >> $2
+	host $i | grep "has address" | awk -F" " '{ print $4 }' >> $2
 done
